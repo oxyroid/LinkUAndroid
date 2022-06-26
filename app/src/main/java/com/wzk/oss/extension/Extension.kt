497ae243c52188c83f03a6df67ea533a5463cd9b
ThxBro:P <1365427005@qq.com>
2022-06-26 10:58:36 +0800

    update surface color change style of snackbar

--- app/src/main/java/com/wzk/oss/extension/Extension.kt
+++ app/src/main/java/com/wzk/oss/extension/Extension.kt
@@ -1,6 +1,9 @@
 package com.wzk.oss.extension
 
+import androidx.compose.material.DrawerState
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 operator fun Color.times(alpha: Float): Color {
     return this.copy(alpha = alpha)
@@ -9,4 +12,11 @@ operator fun Color.times(alpha: Float): Color {
 inline fun <R> Boolean?.ifTrue(block: () -> R): R? {
     return if (this == true) block()
     else null
+}
+
+fun DrawerState.toggle(coroutineScope: CoroutineScope) {
+    coroutineScope.launch {
+        if (isOpen) close()
+        else open()
+    }
 }
--- app/src/main/java/com/wzk/oss/ui/MaterialSnackHost.kt
+++ app/src/main/java/com/wzk/oss/ui/MaterialSnackHost.kt
@@ -1,6 +1,5 @@
 package com.wzk.oss.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -8,10 +7,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.SnackbarHost as SnackbarHostM3
-import androidx.compose.material3.SnackbarHostState as SnackbarHostStateM3
+import com.wzk.oss.ui.theme.Link
 
 @Composable
 fun MaterialSnackHost(state: SnackbarHostState) {
@@ -21,27 +20,24 @@ fun MaterialSnackHost(state: SnackbarHostState) {
     ) { data ->
         Card(
             shape = RoundedCornerShape(8.dp),
-            border = BorderStroke(
-                1.dp,
-                MaterialTheme.colorScheme.tertiaryContainer
-            ),
+            elevation = 2.dp,
             modifier = Modifier
-                .padding(horizontal = 24.dp)
+                .padding(horizontal = 12.dp)
                 .fillMaxWidth()
-                .height(64.dp),
-            backgroundColor = MaterialTheme.colorScheme.tertiary,
-            contentColor = MaterialTheme.colorScheme.onTertiary
+                .height(48.dp),
+            backgroundColor = MaterialTheme.colorScheme.surface,
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(horizontal = 24.dp),
+                    .padding(start = 24.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = data.message,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.weight(1f),
+                    color = Color.White
                 )
                 data.actionLabel?.also {
                     TextButton(onClick = {
@@ -49,48 +45,7 @@ fun MaterialSnackHost(state: SnackbarHostState) {
                     }) {
                         Text(
                             text = it,
-                            color = MaterialTheme.colorScheme.onTertiary
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun MySnackHostM3(state: SnackbarHostStateM3) {
-    SnackbarHostM3(
-        hostState = state,
-        modifier = Modifier.padding(vertical = 12.dp)
-    ) { data ->
-        Card(
-            shape = RoundedCornerShape(8.dp),
-            modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .fillMaxWidth()
-                .height(64.dp),
-            backgroundColor = MaterialTheme.colorScheme.tertiary,
-            contentColor = MaterialTheme.colorScheme.onTertiary
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 24.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = data.visuals.message,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.weight(1f),
-                )
-                data.visuals.actionLabel?.also {
-                    TextButton(onClick = {
-                        data.performAction()
-                    }) {
-                        Text(
-                            text = it,
-                            color = MaterialTheme.colorScheme.onTertiary
+                            color = Link
                         )
                     }
                 }
