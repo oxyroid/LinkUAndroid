be67bd8f82dddfa276345cce57b1a35843665b3c
ThxBro:P <1365427005@qq.com>
2022-09-23 17:31:30 +0800

    order: change inner classes order of Strategy.kt

--- domain/src/main/java/com/linku/domain/Strategy.kt
+++ domain/src/main/java/com/linku/domain/Strategy.kt
@@ -1,9 +1,9 @@
 package com.linku.domain
 
 sealed class Strategy {
-    object CacheElseNetwork : Strategy()
-    object NetworkThenCache : Strategy()
+    object Memory : Strategy()
     object OnlyCache : Strategy()
     object OnlyNetwork : Strategy()
-    object Memory : Strategy()
+    object NetworkThenCache : Strategy()
+    object CacheElseNetwork : Strategy()
 }
