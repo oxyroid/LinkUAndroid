6a2fe0a9759208a5dfe760cc9b362b4278022a00
ThxBro:P <1365427005@qq.com>
2022-09-23 15:48:48 +0800

    rename: ChatScreenMode#ImageDetail s to url, and rect to boundaries

--- app/src/main/java/com/linku/im/screen/chat/ChatScreenMode.kt
+++ app/src/main/java/com/linku/im/screen/chat/ChatScreenMode.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.geometry.Rect
 
 sealed class ChatScreenMode {
     object Messages : ChatScreenMode()
-    data class ImageDetail(val s: String, val rect: Rect) : ChatScreenMode()
+    data class ImageDetail(val url: String, val boundaries: Rect) : ChatScreenMode()
     data class MemberDetail(val mid: Int) : ChatScreenMode()
     object ChannelDetail : ChatScreenMode()
 }
