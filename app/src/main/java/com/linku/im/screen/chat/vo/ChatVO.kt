c09ae2dbfcc2dd70ec5a13ad11ac75528526401c
ThxBro:P <1365427005@qq.com>
2022-09-23 15:49:21 +0800

    rename: Rename VO entities

--- app/src/main/java/com/linku/im/screen/chat/vo/ChatVO.kt
+++ app/src/main/java/com/linku/im/screen/chat/vo/ChatVO.kt
@@ -1,26 +1,19 @@
 package com.linku.im.screen.chat.vo
 
-import androidx.annotation.Keep
 import com.linku.data.usecase.UserUseCases
 import com.linku.domain.Strategy
-import com.linku.domain.entity.Message
 import com.linku.im.Constants
 import com.linku.im.extension.ifTrue
-import com.linku.im.screen.chat.composable.BubbleConfig
-
-@Keep
-data class MessageVO(
-    val message: Message,
-    val config: BubbleConfig
-)
 
 sealed class ChatVO {
-    data class Data(
-        val message: Message,
+    data class Message(
+        val message: com.linku.domain.entity.Message,
         val other: Boolean
     ) : ChatVO()
+
     data class TimeSeparator(val timestamp: Long) : ChatVO()
-    data class AvatarSeparator(
+    data class UserSeparator(
+        val uid: Int,
         val avatar: String
     ) : ChatVO()
 }
@@ -28,10 +21,10 @@ sealed class ChatVO {
 infix fun ChatVO?.calculateTimeSeparator(
     after: ChatVO?
 ): ChatVO.TimeSeparator? = when (val before = this) {
-    is ChatVO.Data -> {
+    is ChatVO.Message -> {
         val duration = Constants.CHAT_LABEL_MIN_DURATION
         when (after) {
-            is ChatVO.Data ->
+            is ChatVO.Message ->
                 (after.message.timestamp - before.message.timestamp >= duration).ifTrue {
                     ChatVO.TimeSeparator(after.message.timestamp)
                 }
@@ -39,15 +32,15 @@ infix fun ChatVO?.calculateTimeSeparator(
         }
     }
     is ChatVO.TimeSeparator -> null
-    is ChatVO.AvatarSeparator -> null
+    is ChatVO.UserSeparator -> null
     null -> when (after) {
-        is ChatVO.Data -> ChatVO.TimeSeparator(after.message.timestamp)
+        is ChatVO.Message -> ChatVO.TimeSeparator(after.message.timestamp)
         else -> null
     }
 }
 
 /**
- * If after is already [ChatVO.AvatarSeparator] => null.
+ * If after is already [ChatVO.UserSeparator] => null.
  * If before is not data => null.
  * If before is data and after is [ChatVO.TimeSeparator] => separator.
  * If before and after are data and before is other but after is not other => separator.
@@ -56,17 +49,18 @@ infix fun ChatVO?.calculateTimeSeparator(
 
 context(UserUseCases) suspend infix fun ChatVO?.calculateAvatarSeparator(
     after: ChatVO?
-): ChatVO.AvatarSeparator?  {
+): ChatVO.UserSeparator? {
     val before = this
     suspend fun separator(
         uid: Int
-    ): ChatVO.AvatarSeparator = ChatVO.AvatarSeparator(
+    ): ChatVO.UserSeparator = ChatVO.UserSeparator(
+        uid,
         findUser(uid, Strategy.Memory).let { it?.avatar.orEmpty() },
     )
     return when {
-        after is ChatVO.AvatarSeparator -> null
-        before is ChatVO.Data -> when(after) {
-            is ChatVO.Data ->
+        after is ChatVO.UserSeparator -> null
+        before is ChatVO.Message -> when (after) {
+            is ChatVO.Message ->
                 if (before.other && !after.other) separator(before.message.uid)
                 else null
             is ChatVO.TimeSeparator -> separator(before.message.uid)
