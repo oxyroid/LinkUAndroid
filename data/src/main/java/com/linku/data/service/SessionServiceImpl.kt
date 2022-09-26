854eca2e5047c55db009f801280467f085eb7e63
ThxBro:P <1365427005@qq.com>
2022-09-27 00:28:02 +0800

    remove: useless packages and classes

--- data/src/main/java/com/linku/data/service/SessionServiceImpl.kt
+++ data/src/main/java/com/linku/data/service/SessionServiceImpl.kt
@@ -1,9 +1,5 @@
 package com.linku.data.service
 
-import android.util.Log
-import com.linku.data.TAG
-import com.linku.data.debug
-import com.linku.data.error
 import com.linku.domain.entity.Message
 import com.linku.domain.entity.MessageDTO
 import com.linku.domain.service.SessionService
@@ -12,8 +8,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.MissingFieldException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.*
@@ -25,8 +19,6 @@ class SessionServiceImpl @Inject constructor(
     private val json: Json
 ) : SessionService {
     private val incoming = MutableSharedFlow<Message>()
-
-    @OptIn(ExperimentalSerializationApi::class)
     override fun initSession(uid: Int?): Flow<SessionService.State> = callbackFlow {
         trySend(SessionService.State.Connecting)
         if (uid == null) {
@@ -39,20 +31,15 @@ class SessionServiceImpl @Inject constructor(
         val socket = client.newWebSocket(request, object : WebSocketListener() {
             override fun onOpen(webSocket: WebSocket, response: Response) {
                 super.onOpen(webSocket, response)
-                error("Open")
                 trySend(SessionService.State.Connected(webSocket))
             }
 
             override fun onMessage(webSocket: WebSocket, text: String) {
                 super.onMessage(webSocket, text)
-                error("message")
                 launch {
                     try {
                         json.decodeFromString<SocketPackage<MessageDTO>>(text).data.toMessage()
-                    } catch (e: MissingFieldException) {
-                        null
                     } catch (e: Exception) {
-                        error(e.message)
                         null
                     }?.also {
                         incoming.emit(it)
@@ -66,15 +53,10 @@ class SessionServiceImpl @Inject constructor(
 
             override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
                 super.onClosing(webSocket, code, reason)
-                error("closing")
             }
 
             override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
                 super.onClosed(webSocket, code, reason)
-                error("closed")
-                debug {
-                    Log.e(TAG, "Websocket closed, code = $code, reason = $reason")
-                }
                 launch {
                     trySend(SessionService.State.Closed)
                 }
@@ -82,9 +64,6 @@ class SessionServiceImpl @Inject constructor(
 
             override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
                 super.onFailure(webSocket, t, response)
-                debug {
-                    Log.e(TAG, "Websocket failed", t)
-                }
                 trySend(SessionService.State.Failed(t.message))
             }
         })
