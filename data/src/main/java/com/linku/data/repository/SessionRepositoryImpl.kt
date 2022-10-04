47c137bdd69bb56ab269720c8c1bd7c8c2cae1bb
ThxBro:P <1365427005@qq.com>
2022-10-04 16:39:11 +0800

    resource: use string res instead of string.

--- data/src/main/java/com/linku/data/repository/SessionRepositoryImpl.kt
+++ data/src/main/java/com/linku/data/repository/SessionRepositoryImpl.kt
@@ -1,16 +1,18 @@
 package com.linku.data.repository
 
+import com.linku.data.R
+import com.linku.data.usecase.ApplicationUseCases
 import com.linku.domain.Resource
 import com.linku.domain.entity.toConversation
 import com.linku.domain.repository.SessionRepository
 import com.linku.domain.repository.SessionRepository.State.*
+import com.linku.domain.resultOf
 import com.linku.domain.room.dao.ConversationDao
 import com.linku.domain.room.dao.MessageDao
 import com.linku.domain.service.AuthService
 import com.linku.domain.service.ConversationService
 import com.linku.domain.service.NotificationService
 import com.linku.domain.service.SessionService
-import com.linku.domain.toResult
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import okhttp3.WebSocket
@@ -22,7 +24,8 @@ class SessionRepositoryImpl @Inject constructor(
     private val messageDao: MessageDao,
     private val conversationDao: ConversationDao,
     private val conversationService: ConversationService,
-    private val notificationService: NotificationService
+    private val notificationService: NotificationService,
+    private val applications: ApplicationUseCases
 ) : SessionRepository {
     private var session: WebSocket? = null
     private val sessionState = MutableStateFlow<SessionRepository.State>(Default)
@@ -51,9 +54,12 @@ class SessionRepositoryImpl @Inject constructor(
                     SessionService.State.Connecting -> Resource.Loading
                     is SessionService.State.Connected -> Resource.Success(Unit)
                     SessionService.State.Closed -> Resource.Success(Unit)
-                    is SessionService.State.Failed -> Resource.Failure(
-                        state.reason ?: "Cannot initialize this session."
-                    )
+                    is SessionService.State.Failed -> {
+                        val msg = applications.getString(R.string.error_session_initialize)
+                        Resource.Failure(
+                            state.reason ?: msg
+                        )
+                    }
                 }
             }
             .distinctUntilChanged()
@@ -62,11 +68,13 @@ class SessionRepositoryImpl @Inject constructor(
     override fun subscribeRemote(): Flow<Resource<Unit>> = channelFlow {
         trySend(Resource.Loading)
         if (sessionState.value != Connected) {
-            trySend(Resource.Failure("You must subscribe it before the session has been initialized."))
+            val msg = applications.getString(R.string.error_session_subscribe)
+            trySend(Resource.Failure(msg))
         }
         sessionState.tryEmit(Subscribing)
-        authService.subscribe()
-            .toResult()
+        resultOf {
+            authService.subscribe()
+        }
             .onSuccess {
                 sessionState.tryEmit(Subscribed)
                 trySend(Resource.Success(Unit))
@@ -77,13 +85,18 @@ class SessionRepositoryImpl @Inject constructor(
                         val cid = it.cid
                         if (conversationDao.getById(it.id) == null) {
                             launch {
-                                conversationService.getConversationById(cid)
-                                    .toResult()
+                                resultOf {
+                                    conversationService.getConversationById(cid)
+                                }
                                     .onSuccess { dto ->
                                         conversationDao.insert(dto.toConversation())
                                     }
                                     .onFailure {
-                                        error("Cannot to save conversation, cid = $cid")
+                                        val msg = applications.getString(
+                                            R.string.error_save_conversation,
+                                            cid
+                                        )
+                                        error(msg)
                                     }
                             }
                         }
