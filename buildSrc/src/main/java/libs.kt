1a7994c4bf925e5a70ef70e2def2af88c9e5ae9f
ThxBro:P <1365427005@qq.com>
2022-11-20 02:01:37 +0800

    Remove useless coding and resources

--- buildSrc/src/main/java/libs.kt
+++ buildSrc/src/main/java/libs.kt
@@ -1,8 +1,5 @@
 object libs {
     object plugins {
-        const val agp_version = "7.3.1"
-        const val kotlin_version = "1.8.0-Beta"
-        const val detekt_version = "1.22.0-RC3"
 
         object android {
             const val application = "com.android.application"
